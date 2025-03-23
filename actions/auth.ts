67cc3052e3390e9f3c0a9126656f57574aad901d
Sabelokhoza <161332464+Sabelokhoza@users.noreply.github.com>
2025-03-23 09:20:30 +0200

    added sign-in functionality

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,1 +1,47 @@
 "use server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/superbase/server";
+import { headers } from "next/headers";
+
+export async function signUp(formData: FormData) {
+    const superbase = await createClient();
+
+    const credentials = {
+        username : formData.get("username") as string,
+        email : formData.get("email") as string,
+        password : formData.get("password") as string,
+    }
+
+    const {error,data} = await superbase.auth.signUp({
+        email: credentials.email,
+        password: credentials.password,
+        options: {
+          data: {
+            username: credentials.username,
+          },
+        },
+      })
+
+      if(error){
+        return {
+            status : error?.message,
+            user : null
+          };
+      }else if(data?.user?.identities === 0){
+
+        return {
+            status : "user with this email already exists, please log in",
+            user : null
+          };
+      }
+
+      revalidatePath("/","layout");
+      return{
+        status : "success",
+        user : data.user
+      }
+
+
+}
