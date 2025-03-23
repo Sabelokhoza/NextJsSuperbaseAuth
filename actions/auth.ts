e1904a47efe93175c7ce8f45a747894f7ad7660f
Sabelokhoza <161332464+Sabelokhoza@users.noreply.github.com>
2025-03-23 09:33:46 +0200

    added sign-in functionality

--- actions/auth.ts
+++ actions/auth.ts
@@ -6,42 +6,68 @@ import { createClient } from "@/utils/superbase/server";
 import { headers } from "next/headers";
 
 export async function signUp(formData: FormData) {
-    const superbase = await createClient();
+  const superbase = await createClient();
 
-    const credentials = {
-        username : formData.get("username") as string,
-        email : formData.get("email") as string,
-        password : formData.get("password") as string,
-    }
+  const credentials = {
+    username: formData.get("username") as string,
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
 
-    const {error,data} = await superbase.auth.signUp({
-        email: credentials.email,
-        password: credentials.password,
-        options: {
-          data: {
-            username: credentials.username,
-          },
-        },
-      })
+  const { error, data } = await superbase.auth.signUp({
+    email: credentials.email,
+    password: credentials.password,
+    options: {
+      data: {
+        username: credentials.username,
+      },
+    },
+  });
 
-      if(error){
-        return {
-            status : error?.message,
-            user : null
-          };
-      }else if(data?.user?.identities === 0){
+  if (error) {
+    return {
+      status: error?.message,
+      user: null,
+    };
+  } else if (data?.user?.identities === 0) {
+    return {
+      status: "user with this email already exists, please log in",
+      user: null,
+    };
+  }
 
-        return {
-            status : "user with this email already exists, please log in",
-            user : null
-          };
-      }
+  revalidatePath("/", "layout");
+  return {
+    status: "success",
+    user: data.user,
+  };
+}
 
-      revalidatePath("/","layout");
-      return{
-        status : "success",
-        user : data.user
-      }
+export async function signIn(formData: FormData) {
+  const superbase = await createClient();
 
+  const credentials = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
 
+  const { error, data } = await superbase.auth.signInWithPassword(credentials);
+
+  console.log(error);
+  console.log(credentials)
+
+  if (error) {
+    return {
+      status: error?.message,
+      user: null,
+    };
+  }
+
+  //TODO  create a user instance on user Profiles table
+
+  revalidatePath("/", "layout");
+  return {
+    status: "success",
+    user: data.user,
+  };
 }
